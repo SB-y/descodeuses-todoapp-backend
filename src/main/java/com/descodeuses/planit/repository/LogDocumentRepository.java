431d963469776ae75728e2f6c0239fd22e1a4663
SB-y <bsarah192@icloud.com>
2026-02-21 08:39:45 +0100

    modifs logdocumentrepo à exclure pour test

--- src/main/java/com/descodeuses/planit/repository/LogDocumentRepository.java
+++ src/main/java/com/descodeuses/planit/repository/LogDocumentRepository.java
@@ -1,9 +1,12 @@
 package com.descodeuses.planit.repository;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.descodeuses.planit.entity.LogDocument;
 
+
+@Profile("!test")
 public interface LogDocumentRepository extends MongoRepository<LogDocument, String> {
     
 }
