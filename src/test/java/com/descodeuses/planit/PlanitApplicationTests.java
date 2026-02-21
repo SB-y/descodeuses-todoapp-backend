aa57d3ea835e3ff66515d786616adebdd9ca2a7a
SB-y <bsarah192@icloud.com>
2026-02-21 08:31:00 +0100

    db h2 pour test

--- src/test/java/com/descodeuses/planit/PlanitApplicationTests.java
+++ src/test/java/com/descodeuses/planit/PlanitApplicationTests.java
@@ -2,12 +2,14 @@ package com.descodeuses.planit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PlanitApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
