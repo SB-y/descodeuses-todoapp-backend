2fe9658782208b362c5e4784168e25aa2b75a0d4
SB-y <bsarah192@icloud.com>
2026-02-21 08:43:38 +0100

    modifs pour exclure mongo pour test

--- src/test/java/com/descodeuses/planit/PlanitApplicationTests.java
+++ src/test/java/com/descodeuses/planit/PlanitApplicationTests.java
@@ -4,7 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(
+    properties = {
+        "spring.autoconfigure.exclude=" +
+        "org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration," +
+        "org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration"
+    }
+)
 @ActiveProfiles("test")
 class PlanitApplicationTests {
 
