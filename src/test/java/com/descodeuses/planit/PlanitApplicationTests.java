5f7a946f292869b30b8ed6f677b2e21a6a49fbd2
SB-y <bsarah192@icloud.com>
2026-02-21 08:48:46 +0100

    modifs app test properties mongo

--- src/test/java/com/descodeuses/planit/PlanitApplicationTests.java
+++ src/test/java/com/descodeuses/planit/PlanitApplicationTests.java
@@ -4,18 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(
-    properties = {
-        "spring.autoconfigure.exclude=" +
-        "org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration," +
-        "org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration"
-    }
-)
+@SpringBootTest
 @ActiveProfiles("test")
 class PlanitApplicationTests {
 
     @Test
     void contextLoads() {
     }
+
 
 }
